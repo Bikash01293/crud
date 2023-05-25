a0133e925035ff0117b7e2064bdecd49839a3be8
Bikash Adhikari <bikashadhikari@Bikashs-MacBook-Air.local>
2023-05-25 09:31:08 +0530

    added orm congig in appmodule.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,31 @@ import { AuthInterceptor } from './interceptor/auth.interceptor';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(),
-    //added to the imports array
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: process.env.DATABASE_TYPE,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        synchronize: false,
+        logging: false,
+        entities: ['dist/**/entities/*.entity.{ts,js}'],
+        migrations: ['dist/migration/**/*.{js,ts}'],
+        subscribers: ['src/subscriber/**/*.{js,ts}'],
+        cli: {
+          entitiesDir: 'src/entity',
+          migrationsDir: 'src/migration',
+          subscribersDir: 'src/subscriber',
+        },
+      }),
+    }),
     BooksModule,
     AuthModule,
     UsersModule,
   ],
+  //added to the imports array
   controllers: [AppController],
   providers: [
     AppService,
